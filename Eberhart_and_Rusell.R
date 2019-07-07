10b58432cda77ffd87a7b2cd80a5bafa3d0a083a
Eustrain <eustrain.rs@gmail.com>
2019-07-07 13:37:37 -0500

    add finlay and wilkson

--- Eberhart_and_Rusell.R
+++ Eberhart_and_Rusell.R
@@ -1,25 +1,23 @@
 ##########Eberhart and Russell Model of stability
-
+##E_R(Enviroment,Rep,Entry,y,"name of our variable)
 
 ###Example 1
 dat <- read.csv("E-R.csv",head=T)
 names(dat) <- c("","Entry","Env","Rep","y")
 head(dat)
-
+dat <- dplyr::select(dat,Entry ,Env ,Rep,y)
 
 ####Example 2
 #dat<- read.csv("dat.csv",head=T)
 #E_R(dat$env,dat$rep,dat$geno,dat$y)
-
+attach(dat)
 
     
-
-
-ige<- E_R(dat$Env,dat$Rep,dat$Entry,dat$y)
+ige<- E_R(Env,Rep,Entry,y,"y")
 ige$Stability
 
 
-E_R <- function(Env,Rep,Entry,y){
+E_R <- function(Env,Rep,Entry,y,trait){
 
 ################# Begin
 
@@ -34,26 +32,23 @@ r <- length(levels(Rep))
 s <- length(levels(Loc))
 t <- length(levels(Entry))
 
-##########################
+##########################@@@@@
 m1<- aov(y ~ Entry+ Loc + Loc: Entry)
 anv1 <- anova(m1)
-S2<- anv1$`Mean Sq`[4]
-
-
-#################################################################
+#######################################################
 mydf = data.frame(aggregate (y ~ Entry + Loc,FUN=mean))
 mydf$Loc <- as.factor(mydf$Loc)
 mydf$Entry <- as.factor(mydf$Entry)
 m2 <- aov(y~Entry+Loc+Loc:Entry,data = mydf)
 anv2 <- anova(m2)
-###########
+###############################################
 Lxg <- tapply(y, list(Entry,Loc), mean)
 Lmean <- apply(Lxg,2,mean)
 Lsum <- apply(Lxg,2,sum)
 Emean <- apply(Lxg,1,mean)
 over.mean <- mean(Lxg)
 
-#############################
+################Stability Analisys
 I <- Lmean-over.mean
 I2 <- sum(I^2)
 YijI <- Lxg%*%I
@@ -65,10 +60,11 @@ VarS<- Y2ij-Y2
 bi_I <- bi*YijI 
 dij2 <-VarS -bi_I
 
-####################
+####################@@@@@@@@@@@@@@@@@@@@@
 s2 <- anv1$`Mean Sq`[4]
 sdi <- (dij2/(s-2))-(s2/r)
-#####################################
+#######################Sum Square
+
 SS.varieties <-anv2$`Sum Sq`[1]
 SS.Env_varietiesxenv<- sum(Y2ij)-sum(apply(Lxg, 1,sum)^2)/s
 SS.enviromen_linear <- anv2$`Sum Sq`[2]
@@ -79,7 +75,7 @@ ss.sd <- dij2
 SS.poolerr <-  anv1 $"Sum Sq"[4] / r
 SS <-c(SS.varieties,SS.Env_varietiesxenv,SS.enviromen_linear,SS.varietiesxenviroment_linar
        ,SS.pool.desviation,ss.sd,SS.poolerr)
-######################################
+########################### degree freedon
 df_varieties<- t-1
 df_Env_varietiesxenv<- t*(s-1)
 df_enviromen_linear <- 1
@@ -89,7 +85,7 @@ df_sd <- rep(s-2,length(dij2))
 df_pooerr <- s*t*(r-1)
 df <- c(df_varieties,df_Env_varietiesxenv,df_enviromen_linear,df_varietiesxenviroment_linar
         ,df_pool.desviation,df_sd,df_pooerr)
-##################################
+#########################Mean Square
 
 
 ms_varieties<- SS.varieties /df_varieties
@@ -119,16 +115,17 @@ anovadf <- data.frame(df, `Sum Sq`=SS, `Mean Sq`=ms, `F value`= ft,`Pr(>F)`= pva
 rownames(anovadf) <- c("Genotypes","Env + (Gen x Env)", "Env (linear)", " Gen x Env(linear)", "Pooled deviation",levels(Entry), "Pooled error" ) 
 class(anovadf) <- c("anova","data.frame")
 
-###########stability
+###########stability Anova
 
 stb <- data.frame("Genotype"=Entry_names,"Means"=Emean,"bi"=bi,"Sdi"=sdi)
 stb
 
 cat(" Stable genotype have bij = 1 and sdij = 0", "\n\n"  )
+n <- c("y")
 
-
+ 
 plot(1, type = 'n', xlim = range(c(stb[,3])) ,ylim = range(c(stb[,3], stb[,4])), xlab = "bi",
-     ylab ="Sdi")
+     ylab ="Sdi",main = paste("Eberhart-Russell`s Model of stability for ", trait, sep = ""))
 
 text(stb[,3], stb[,4], labels = Entry_names, adj = c(0.5, 0.5), col = "blue")
 abline(h = 0, v=1,lty = 1)
---
+++ Finlay_Wilkinson.R
@@ -0,0 +1,100 @@
+### Regression Analysis of Stability 
+### (Finlay and Wilkinson's Model)
+###
+###Finlay and Wilkinson defined that b = 1.0 indicates
+###average ','stability" , b < 1.0 means " below average stability" and b > 
+###1,0 suggests "above average stability". 
+
+
+
+
+dat <- read.csv("F-W.csv",header = T)
+
+dat <- dplyr::select(dat,Entry,Loc,Rep,y)
+names(dat) <- c("Entry","Env","Rep","y")
+head(dat)
+
+
+
+f-W<- fw(dat$Entry,dat$Env,dat$Rep,dat$y,"Yield")
+
+
+
+fw<- function(Entry,Env,Rep,y,trait){
+
+
+Entry <-as.factor (Entry)
+Loc <- as.factor(Env)
+rep <- as.factor(Rep)
+y <- as.numeric(y)
+geno.num <- nlevels(Entry)
+env.num <- nlevels(Loc)
+rep.num <- nlevels(rep)
+Entry_names<- levels(Entry)
+
+
+#####################anova
+
+m1 <- aov(y~Entry+Loc+rep:Loc+Entry:Loc)
+an1 <- anova(m1)
+
+
+################# Regression anovo
+
+int.mean <- tapply(y,list(Entry,Loc), mean)
+
+overall.mean <- mean(int.mean, na.rm=T)
+env.mean <- apply(int.mean, 2, mean, na.rm=T)
+geno.mean <- apply(int.mean, 1, mean, na.rm=T)
+
+
+rgss<- apply(int.mean, 2, function(x){
+  x/mean(x)
+})
+
+
+RGss<- (apply(rgss, 1, sum)^2)
+RGss <- (rep.num)^2*(sum(RGss))
+Rgms <- RGss/(geno.num-1)
+Drss <- an1$`Sum Sq`[4]-RGss
+Drms <- Drss/((geno.num-1)*(env.num-1)-(rep.num-1))
+SS <- c(RGss,Drss )
+MS <- c(Rgms,Drms)
+f <- MS/SS
+d_f <- c((geno.num-1),((geno.num-1)*(env.num-1)-(rep.num-1)))
+pr <-  c(pf(f[1],d_f[1], an1$Df[5]),pf(f[2],d_f[2], an1$Df[5]))
+
+DF <- data.frame(`Df`=d_f,`Sum sq`=SS,`Ms sq`=MS,F=f,`Pr(>F)`= pr,check.names=FALSE)
+rownames(DF) <- c("Regression","Dev.Regr") 
+class(DF) <- c("anova","data.frame")
+
+
+### Regression-stability for environments
+
+
+b <- NULL
+se <- NULL
+
+for (i in 1:geno.num){
+  modelo <- lm(int.mean[i,] ~ env.mean)
+  b[i] <- coef(modelo)[2]
+  se[i] <- summary.lm(modelo)$coefficients[2,2]
+}
+
+stability <- cbind(geno.mean ,b, se)
+stability <- as.data.frame(stability )
+names(stability) <- c("Genotype","bi","SE")
+
+
+plot(1, type = 'n', xlim = range(c(stability[,1])) ,ylim = range(stability[,2]), xlab = trait,
+     ylab ="bi",main=paste("Finlay and Wilkinson's Model for ", trait, sep = ""))
+
+text(stability[,1], stability[,2], labels = Entry_names, adj = c(0.5, 0.5), col = "blue",pos = 3)
+points(stability[,1], stability[,2], col = "black", lwd = 2, pch =16)
+abline(h = 1, v=overall.mean ,lty = 1)
+
+result <- list(ANOVA=an1,FIW=DF,stability=stability)
+
+return(result)
+
+}
